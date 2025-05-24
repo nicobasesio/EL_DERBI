6db77a07c8ffec1a17d6503f4d5ff75bae5623f2
martacuellar <marta.cuellar@alumnos.upm.es>
2025-05-24 19:50:49 +0200

    Update caballo.h

--- src/caballo.h
+++ src/caballo.h
@@ -2,5 +2,20 @@
 #include "pieza.h"
 class Caballo : public Pieza
 {
+<<<<<<< Updated upstream
+=======
+private:
+	Raton r;
+	bool color; // false rojo; true negro
+	bool haComidoPieza;
+	ETSIDI::SpriteSequence sprite{ "imagenes/caballoB.png",1 };
+	ETSIDI::SpriteSequence sprite2{ "imagenes/caballoR.png",1 };
+
+public:
+	caballo() 
+	{
+		sprite.setCenter
+	}
+>>>>>>> Stashed changes
 };
 
