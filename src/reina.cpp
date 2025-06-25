f2c563bae5c5b1083ddf3bfdf9b6bae8b2bda5ae
rodricp2504 <rodrigo.cancela@alumnos.upm.es>
2025-06-25 10:52:43 +0200

    cambios2

--- src/reina.cpp
+++ src/reina.cpp
@@ -12,7 +12,7 @@ void Reina::muevepieza(double x, double y)
 }
 
 
-bool Reina::casillaValida(int i, int j, std::vector<std::vector<Pieza*>> control) {   // Para que no se salga del tamaño de la matriz control al comprobar si es jaque o no
+bool Reina::casillaValida(int i, int j, std::vector<std::vector<Pieza*>> control) {   
 	if (i >= 0 && i < control.size() && j >= 0 && j < control[0].size())
 		return true;
 	else
@@ -21,151 +21,7 @@ bool Reina::casillaValida(int i, int j, std::vector<std::vector<Pieza*>> control
 }
 
 
-std::vector<VECTOR2D> Reina::get_movimientos_validos(std::vector<std::vector<Pieza*>> control, VECTOR2D pos, VECTOR2D reyPos) { // pos es la casilla que ocupa la torre en nuestra matriz de control
-	haComidoPieza = false; // por defecto no come pieza. En caso de que tenga la posibilidad de hacerlo, si valor cambiará a true
-	std::vector<VECTOR2D> posiciones; // se almacenan las posiciones de control de todas las piezas de color negro
-	//FILAS REINA (horizontal)
-	for (int i = 1; i < control.size(); i++) { //HORIZONTAL derecha DE LA TORRE
-		if (casillaValida(pos.x + i, pos.y, control)) { // se comprueba que no se salga del rango de la matriz
-			if (control[pos.x + i][pos.y] == nullptr) { // se recorre la fila de la torre en horizontal (y = cte)
-				posiciones.push_back({ static_cast<double>(pos.x + i), static_cast<double>(pos.y) }); // se añade al vector las posiciones donde la torre puede moverse
-				//haComidoPieza = false;
-			}
-			else if (control[pos.x + i][pos.y] != nullptr && control[pos.x + i][pos.y]->get_color() == true) { //se comprueba que es viable la captura de la pieza
-				posiciones.push_back({ static_cast<double>(pos.x + i), static_cast<double>(pos.y) }); // se añade al vector las posiciones donde la torre puede moverse
-				haComidoPieza = true;
-				break;
-			}
-			else if (control[pos.x + i][pos.y] != nullptr && control[pos.x + i][pos.y]->get_color() == false) { // se detecta pieza negra
-				//haComidoPieza = false;
-				break;
-			}
-		}
-	}
-	for (int i = 1; i < control.size(); i++) { 		//HORIZONTAL izquierda DE LA TORRE
-		if (casillaValida(pos.x - i, pos.y, control)) { // se comprueba que no se salga del rango de la matriz
-			if (control[pos.x - i][pos.y] == nullptr) { // se recorre la fila de la torre en horizontal (y = cte)
-				posiciones.push_back({ static_cast<double>(pos.x - i), static_cast<double>(pos.y) }); // se añade al vector las posiciones donde la torre puede moverse
-				//haComidoPieza = false;
-			}
-			else if (control[pos.x - i][pos.y] != nullptr && control[pos.x - i][pos.y]->get_color() == true) {
-				posiciones.push_back({ static_cast<double>(pos.x - i), static_cast<double>(pos.y) }); // se añade al vector las posiciones donde la torre puede moverse
-				haComidoPieza = true;
-				break;
-			}
-			else if (control[pos.x - i][pos.y] != nullptr && control[pos.x - i][pos.y]->get_color() == false) { // se detecta pieza negra
-				//haComidoPieza = false; 
-				break;
-			}
-		}
-	}
-	//COLUMNAS REINA (vertical)
-	for (int i = 1; i < control[0].size(); i++) { // tamaño de la vertical 
-		if (casillaValida(pos.x, pos.y + i, control)) { // se comprueba que no se salga del rango de la matriz
-			if (control[pos.x][pos.y + i] == nullptr) { // se recorre la fila de la torre en horizontal (y = cte)
-				posiciones.push_back({ static_cast<double>(pos.x), static_cast<double>(pos.y + i) }); // se añade al vector las posiciones donde la torre puede moverse
-				//haComidoPieza = false;
-			}
-			else if (control[pos.x][pos.y + i] != nullptr && control[pos.x][pos.y + i]->get_color() == true) {
-				posiciones.push_back({ static_cast<double>(pos.x), static_cast<double>(pos.y + i) }); // se añade al vector las posiciones donde la torre puede moverse
-				haComidoPieza = true;
-				break;
-			}
-			else if (control[pos.x][pos.y + i] != nullptr && control[pos.x][pos.y + i]->get_color() == false) { // se detecta pieza roja
-				//haComidoPieza = false; 
-				break;
-			}
-		}
-	}
-	for (int i = 1; i < control[0].size(); i++) { //VERTICAL abajo DE LA TORRE
-		if (casillaValida(pos.x, pos.y - i, control)) { // se comprueba que no se salga del rango de la matriz
-			if (control[pos.x][pos.y - i] == nullptr) { // se recorre la fila de la torre en horizontal (y = cte)
-				posiciones.push_back({ static_cast<double>(pos.x), static_cast<double>(pos.y - i) }); // se añade al vector las posiciones donde la torre puede moverse
-				//haComidoPieza = false;
-			}
-			else if (control[pos.x][pos.y - i] != nullptr && control[pos.x][pos.y - i]->get_color() == true) {
-				posiciones.push_back({ static_cast<double>(pos.x), static_cast<double>(pos.y - i) }); // se añade al vector las posiciones donde la torre puede moverse
-				haComidoPieza = true;
-				break;
-			}
-			else if (control[pos.x][pos.y - i] != nullptr && control[pos.x][pos.y - i]->get_color() == false) { // se detecta pieza roja
-				//haComidoPieza = false;
-				break;
-			}
-		}
-	}
-	//1ª y 3ª DIAGONAL REINA (arriba derecha. Abajo izda)
-	for (int i = 1; i < control.size(); i++) { //1ª diagonal
-		if (casillaValida(pos.x + i, pos.y + i, control)) { // se comprueba que no se salga del rango de la matriz
-			if (control[pos.x + i][pos.y + i] == nullptr) { // se recorre la fila de la torre en horizontal (y = cte)
-				posiciones.push_back({ static_cast<double>(pos.x + i), static_cast<double>(pos.y + i) }); // se añade al vector las posiciones donde la torre puede moverse
-				//haComidoPieza = false;
-			}
-			else if (control[pos.x + i][pos.y + i] != nullptr && control[pos.x + i][pos.y + i]->get_color() == true) { //se comprueba que es viable la captura de la pieza
-				posiciones.push_back({ static_cast<double>(pos.x + i), static_cast<double>(pos.y + i) }); // se añade al vector las posiciones donde la torre puede moverse
-				haComidoPieza = true;
-				break;
-			}
-			else if (control[pos.x + i][pos.y + i] != nullptr && control[pos.x + i][pos.y + i]->get_color() == false) { // se detecta pieza roja
-				//haComidoPieza = false;
-				break;
-			}
-		}
-	}
-	for (int i = 1; i < control.size(); i++) { 		//3ª diagonal
-		if (casillaValida(pos.x - i, pos.y - i, control)) { // se comprueba que no se salga del rango de la matriz
-			if (control[pos.x - i][pos.y - i] == nullptr) { // se recorre la fila de la torre en horizontal (y = cte)
-				posiciones.push_back({ static_cast<double>(pos.x - i), static_cast<double>(pos.y - i) }); // se añade al vector las posiciones donde la torre puede moverse
-				//haComidoPieza = false;
-			}
-			else if (control[pos.x - i][pos.y - i] != nullptr && control[pos.x - i][pos.y - i]->get_color() == true) {
-				posiciones.push_back({ static_cast<double>(pos.x - i), static_cast<double>(pos.y - i) }); // se añade al vector las posiciones donde la torre puede moverse
-				haComidoPieza = true;
-				break;
-			}
-			else if (control[pos.x - i][pos.y - i] != nullptr && control[pos.x - i][pos.y - i]->get_color() == false) { // se detecta pieza roja
-				//haComidoPieza = false;
-				break;
-			}
-		}
-	}
-	//2ª y 4ª DIAGONAL REINA (arriba izqda. Abajo drcha)
-	for (int i = 1; i < control.size(); i++) { //2ª diagonal
-		if (casillaValida(pos.x - i, pos.y + i, control)) { // se comprueba que no se salga del rango de la matriz
-			if (control[pos.x - i][pos.y + i] == nullptr) { // se recorre la fila de la torre en horizontal (y = cte)
-				posiciones.push_back({ static_cast<double>(pos.x - i), static_cast<double>(pos.y + i) }); // se añade al vector las posiciones donde la torre puede moverse
-				//haComidoPieza = false;
-			}
-			else if (control[pos.x - i][pos.y + i] != nullptr && control[pos.x - i][pos.y + i]->get_color() == true) { //se comprueba que es viable la captura de la pieza
-				posiciones.push_back({ static_cast<double>(pos.x - i), static_cast<double>(pos.y + i) }); // se añade al vector las posiciones donde la torre puede moverse
-				haComidoPieza = true;
-				break;
-			}
-			else if (control[pos.x - i][pos.y + i] != nullptr && control[pos.x - i][pos.y + i]->get_color() == false) { // se detecta pieza negra
-				//haComidoPieza = false;
-				break;
-			}
-		}
-	}
-	for (int i = 1; i < control.size(); i++) { 		//4ª diagonal
-		if (casillaValida(pos.x + i, pos.y - i, control)) { // se comprueba que no se salga del rango de la matriz
-			if (control[pos.x + i][pos.y - i] == nullptr) { // se recorre la fila de la torre en horizontal (y = cte)
-				posiciones.push_back({ static_cast<double>(pos.x + i), static_cast<double>(pos.y - i) }); // se añade al vector las posiciones donde la torre puede moverse
-				//haComidoPieza = false;
-			}
-			else if (control[pos.x + i][pos.y - i] != nullptr && control[pos.x + i][pos.y - i]->get_color() == true) {
-				posiciones.push_back({ static_cast<double>(pos.x + i), static_cast<double>(pos.y - i) }); // se añade al vector las posiciones donde la torre puede moverse
-				haComidoPieza = true;
-				break;
-			}
-			else if (control[pos.x + i][pos.y - i] != nullptr && control[pos.x + i][pos.y - i]->get_color() == false) { // se detecta pieza roja
-				//haComidoPieza = false;
-				break;
-			}
-		}
-	}
-	return posiciones;
-}
+
 
 void Reina::set_pos_pieza(const VECTOR2D& pos)
 {
@@ -221,7 +77,7 @@ bool Reina::caminoLibre(VECTOR2D origen, VECTOR2D destino, const std::vector<std
 bool Reina::pieza_comible(VECTOR2D casilla_actual, std::vector<std::vector<Pieza*>> control) {
 	int reina_x = casilla_actual.x - 2;
 	int reina_y = casilla_actual.y - 2;
-	for (int dx = 0; dx < 3; ++dx) {  //for para darme las 8 casillas al rededor del rey
+	for (int dx = 0; dx < 3; ++dx) {  //for para darme las 8 casillas alrededor de la reina
 		for (int dy = 0; dy < 3; ++dy) {
 			if (dx == 1 && dy == 1) {
 				continue;
@@ -230,7 +86,7 @@ bool Reina::pieza_comible(VECTOR2D casilla_actual, std::vector<std::vector<Pieza
 			int y = reina_y + dy - 1;
 			if (x >= 0 && x <= 7 && y >= 0 && y <= 8) {  // mira si esta en el limite del tablero
 				if (control[x][y] != nullptr) {
-					//std::cout << "Hay pieza comible para el rey en control[" << x << "," << y << "]\n";
+					
 					return true;
 				}
 			}
